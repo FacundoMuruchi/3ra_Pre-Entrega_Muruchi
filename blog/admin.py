efdf0f712053c27919aae0236eaab509f4600f8d
Facundo Muruchi <149955579+FacundoMuruchi@users.noreply.github.com>
2023-11-08 15:25:35 -0300

    Add files via upload

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Usuario, Poema, Cuento
+
+# Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Poema)
+admin.site.register(Cuento)
